c58652f5aca4d7f339ffd1770b6c4027cbbb51bf
thisisazeez <imfostersyndrome@gmail.com>
2021-11-16 21:32:41 +0100

    *-*

--- app/views.py
+++ app/views.py
@@ -13,12 +13,12 @@ class MembershipView(ListView):
     model = Membership
     template_name = 'list.html'
     def get_user_membership(self):
-        user_membership_qs = UserMembership.objects.filter(user=self.request.user)
+        user_membership_qs = UserMembership.objects.filter()#user=self.request.user
         if user_membership_qs.exists():
             return user_membership_qs.first()
         return None
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        current_membership = self.get_user_membership(self.request)
-        context['current_membership'] = str(current_membership.membership)
+        current_membership = self.get_user_membership()#self.request
+        context['current_membership'] = str()#current_membership.membership
         return context
