fbbca68e95022965a009c99fe3c98c282eefc16e
thisisazeez <imfostersyndrome@gmail.com>
2021-11-16 21:21:35 +0100

    I have Started working on the membership checkout what is done

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+app_name = 'membership'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('memberships/', views.MembershipView.as_view(), name='select'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')  
+
+
+
+from django.views.generic import ListView
+from .models import Membership, UserMembership, Subscription
+class MembershipView(ListView):
+    model = Membership
+    template_name = 'list.html'
+    def get_user_membership(self):
+        user_membership_qs = UserMembership.objects.filter(user=self.request.user)
+        if user_membership_qs.exists():
+            return user_membership_qs.first()
+        return None
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_membership = self.get_user_membership(self.request)
+        context['current_membership'] = str(current_membership.membership)
+        return context
